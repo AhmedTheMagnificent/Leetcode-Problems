0f7e58edc5c8921caba724fc8e2241468233f42f
Ahmed <87165596+AhmedTheMagnificent@users.noreply.github.com>
2025-07-24 22:55:17 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (24.00%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -1,7 +1,14 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        ans = 0
-        for i in range(n + 1):
-            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
-                ans += i
-        return ans
+        def sumDivisibleBy(x):
+            p = n // x
+            return x * p * (p + 1) // 2
+        return (
+            sumDivisibleBy(3) + 
+            sumDivisibleBy(5) + 
+            sumDivisibleBy(7) -
+            sumDivisibleBy(15) - 
+            sumDivisibleBy(21) - 
+            sumDivisibleBy(35) + 
+            sumDivisibleBy(105)  
+        )
